9155af5639c37533027698080c4f698cab3511a4
shanjid9111 <captainlevi9111@gmail.com>
2024-04-01 11:41:41 +0600

    lesson buttons conpleted

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,13 +2,18 @@ import { FeedWrapper } from '@/components/FeedWrapper';
 import { StickyWrapper } from '@/components/StickyWrapper';
 import { Header } from './header';
 import { UserProgress } from '@/components/UserProgress';
-import { getUserProgress } from '@/db/queries';
+import { getUnits, getUserProgress } from '@/db/queries';
 import { redirect } from 'next/navigation';
+import Unit from './Unit';
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) redirect('/courses');
 
@@ -24,6 +29,19 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div className='mb-10' key={unit.id}>
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={undefined}
+              activeLessonPercentage={0}
+            />
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
